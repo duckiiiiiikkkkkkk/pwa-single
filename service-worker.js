1fe4ef701d6c4727df02b2744d746af120e70603
duckiiiiiikkkkkkk <amyamyqwert@gmail.com>
2026-02-23 11:29:48 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,7 @@
-const CACHE_NAME = "single-pwa-v1";
-
-const PRECACHE = [
-  "index.html",
-  "manifest.json",
-  "app.js",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE))
-  );
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => key !== CACHE_NAME && caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
