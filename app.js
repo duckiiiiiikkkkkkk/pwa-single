04f0fb5b8e1f1895e4aa3b85b3045c6f6e8d576b
duckiiiiiikkkkkkk <amyamyqwert@gmail.com>
2026-02-23 11:14:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 function showPage(num) {
   document.getElementById("page1").classList.remove("active");
   document.getElementById("page2").classList.remove("active");
-
   document.getElementById("page" + num).classList.add("active");
 }
 
-// 注册 Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')
       .then(() => console.log("Service Worker 注册成功"))
       .catch(err => console.log("Service Worker 注册失败:", err));
   });
